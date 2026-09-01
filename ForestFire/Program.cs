19b861b35a2a09471c24603699a640697a2fc464
jackjewkes <jackjewkes1@gmail.com>
2026-09-01 19:45:00 +1200

    Initial commit

---
+++ ForestFire/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForestFire
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DrawGrid(4, 4);
+        }
+
+        static void DrawGrid(int width, int height)
+        {
+            Console.Write(string.Concat("+", new string('-', width * 3), "+"));
+            for (int y = 0; y <= height; y++)
+            {
+                Console.Write()
+                for (int x =  0; x <= width; x++)
+                {
+                    Console.Write("s");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ ForestFire/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ForestFire")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ForestFire")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2a05d083-b514-404f-9245-222eaf6105ff")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
